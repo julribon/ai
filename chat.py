992a0852e91462d258df950d3ff7b9d23ec7da2d
Julien Ribon <julien@ribon.ch>
2026-08-11 13:28:42 +0200

    reading WebUI and LMStudio conversations

--- chat.py
+++ chat.py
@@ -1,11 +1,33 @@
+import json
 from src.ai.chat import ollama_chat, lmstudio_chat, huggingface_chat, request_chat
 
 
 prompt = "Quelle est la capitale de la France?"
-print(ollama_chat.get_response(prompt))
-print(lmstudio_chat.get_response(prompt))
+#print(ollama_chat.get_response(prompt))
+#print(lmstudio_chat.get_response(prompt))
 print(request_chat.get_response(prompt, backend="ollama"))
 print(request_chat.get_response(prompt, backend="lmstudio"))
+#print(huggingface_chat.get_response(prompt))
 
-print(huggingface_chat.get_response(prompt))
+print("\nChats Ollama (API):")
+chats = request_chat.list_chats()
+print(chats)
 
+print("\nChats Ollama:")
+chats = request_chat.list_ollama_chats()
+for c in chats:
+    print(f"id:{c['id']}, created:{c['created']:%Y-%m-%d %H:%M}, updated: {c['updated']:%Y-%m-%d %H:%M}, title:{c['title']}")
+
+conversation = request_chat.get_ollama_chat("fe17b4fa-1180-4d8b-b05e-0ed2e440613e")
+print(json.dumps(conversation, indent=2, ensure_ascii=False, default=str))
+
+'''
+print("\nChats LMStudio:")
+chats = request_chat.list_lmstudio_chats()
+#print(chats)
+for c in chats:
+    print(f"id:{c['id']}, created:{c['created_at']:%Y-%m-%d %H:%M}, updated: {c['last_msg_at']:%Y-%m-%d %H:%M}, title:{c['title']}")
+
+conversation = request_chat.get_lmstudio_chat("1786433293794")
+print(json.dumps(conversation, indent=2, ensure_ascii=False, default=str))
+'''
--- src/ai/chat/request_chat.py
+++ src/ai/chat/request_chat.py
@@ -1,4 +1,9 @@
+
 import requests
+import json
+import sqlite3
+from pathlib import Path
+from datetime import datetime
 
 BACKENDS = {
     "lmstudio": ("http://localhost:1234", "mistralai/mistral-small-3.2"),
@@ -15,3 +20,100 @@ def get_response(prompt, backend="ollama"):
     })
 
     return r.json()["choices"][0]["message"]["content"]
+
+
+
+##########################
+
+
+#API_KEY = "ton-token-ici"
+#headers = {"Authorization": f"Bearer {API_KEY}"}
+
+def list_chats():
+    headers = {"Authorization": "Bearer ton-token-ici"}
+    response = requests.get("http://localhost:8080/api/v1/chats/", headers=headers)
+    chats = response.json()
+    return chats
+    '''
+    BASE_URL = "http://localhost:8080"
+    API_KEY = "ton-token-ici"
+
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+    chats = requests.get(f"{BASE_URL}/api/v1/chats/", headers=headers).json()
+
+    for chat in chats:
+        title = chat.get("title", "Sans titre")
+        created_at = datetime.fromtimestamp(chat["created_at"])
+        updated_at = datetime.fromtimestamp(chat["updated_at"])
+        print(f"{title} | créé: {created_at:%Y-%m-%d %H:%M} | maj: {updated_at:%Y-%m-%d %H:%M}")
+    '''
+
+
+DB_PATH = Path.home() / "Library/Application Support/open-webui/data/webui.db"
+
+def list_ollama_chats():
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.execute("SELECT id, title, created_at, updated_at FROM chat ORDER BY created_at DESC")
+
+    chats = []
+    for id, title, created_at, updated_at in cursor.fetchall():
+        created = datetime.fromtimestamp(created_at)
+        updated = datetime.fromtimestamp(updated_at)
+        chats.append({
+            "id": id,
+            "title": title,
+            "created": created,
+            "updated": updated,
+        })
+
+    conn.close()
+    chats.sort(key=lambda chat: chat["created"], reverse=True)
+    
+    return chats
+
+def get_ollama_chat(id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.execute("SELECT chat FROM chat WHERE id = ?", (id,))
+    chats = cursor.fetchall()
+    data = json.loads(chats[0][0]) 
+    conn.close()
+
+    return data
+
+##################################
+
+
+
+CONVERSATIONS_DIR = Path.home() / ".lmstudio" / "conversations"
+
+def list_lmstudio_chats():
+    chats = []
+
+    for filepath in CONVERSATIONS_DIR.glob("*.json"):
+        with open(filepath) as f:
+            data = json.load(f)
+
+        title = data.get("name", "Sans titre")
+        token_count = data["tokenCount"]
+        created_at = datetime.fromtimestamp(data["createdAt"] / 1000)
+        last_msg_at = datetime.fromtimestamp(data["assistantLastMessagedAt"] / 1000)
+
+        chats.append({
+            "title": title,
+            "token_count": token_count,
+            "created_at": created_at,
+            "last_msg_at": last_msg_at,
+            "id": filepath.name.split(".")[0],
+        })
+    
+    chats.sort(key=lambda chat: chat["created_at"], reverse=True)
+    return chats
+
+
+def get_lmstudio_chat(id):
+    filepath = CONVERSATIONS_DIR / f"{id}.conversation.json"
+    data = None
+    with open(filepath) as f:
+        data = json.load(f)
+    return data
